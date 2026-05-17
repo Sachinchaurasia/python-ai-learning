867ca40fe0ec10a31d62dafa93fbf2232ff58d92
Sachin Chaurasia <chaurasiasachin538@gmail.com>
2026-05-17 22:38:18 +0530

    Week 2 Day 5 MatPlotlib(DataVisualizaion)

---
+++ Week2/Matplotlib(DataVisualization)/BasicPlotting.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5]
+y=[10,20,30,40,50]
+
+# plt.plot(x,y)
+# plt.show()
+
+
+## Add Labels to the axes
+
+# plt.plot(x,y)
+# plt.xlabel("x Axis")
+# plt.ylabel("y Axis")
+
+# plt.show()
+
+## Bar Charts  used in Business AI
+
+# names=["A","B","C"]
+# marks=[80,90,70]
+
+# plt.bar(names,marks)
+# plt.show()
+
+## Histograms (Data Distribution)
+
+# data=[10,20,20,30,30,30,40]
+# plt.hist(data)
+# plt.show()
+
+
+## Scatter Plots (Relationship between two variables)
+# x=[1,2,3,4,5]
+# y=[5,7,9,11,13]
+# plt.scatter(x,y)
+# plt.show()
+
+
+## Task 1 Plot line chart for salary Trend over 5 years
+# years=[1,2,3,4,5]
+# salary=[30000,35000,40000,45000,50000]
+
+# plt.plot(years,salary)
+# plt.xlabel("Years")
+# plt.ylabel("Salary")
+# plt.show()
+
+## Bar chart for names and marks(random values)
+# 
+# names=["A","B","C","D"]
+# # 
+# marks=[85,90,80,100]
+# plt.bar(names,marks)
+# plt.xlabel("Names")
+# plt.ylabel("Marks")
+# plt.show()
+
+#  Task 3-histogram for ramdom values of ram in GB
+# ram=[4,8,8,16,16,32]
+# plt.hist(ram)
+# plt.xlabel("RAM in GB")
+# plt.ylabel("Frequency")
+# plt.show()
+
+
+#Task 4- Ai Thinking why visualix=zation should be done before training models
+# Visualization helps in understanding the data distribution, identifying outliers, and gaining insights into the relationships between variables. It allows us to make informed decisions about data preprocessing, feature selection, and model choice, ultimately leading to better model performance and more accurate predictions.
+
