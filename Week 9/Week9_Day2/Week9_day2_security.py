2605d4d5fc4af1ba895742fd6bee67f6aa6fe341
Sachin Chaurasia <chaurasiasachin538@gmail.com>
2026-09-16 23:25:36 +0530

    Week 9_Day2_API_Keys_Environment_Variables_Security

---
+++ Week 9/Week9_Day2/Week9_day2_security.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key=os.getenv("LLM_API_KEY")
+
+if api_key:
+    print("API Key Loaded Successfully")
+else:
+    print("API Key Not Found")
+    
+    
